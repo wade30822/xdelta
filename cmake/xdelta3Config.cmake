fbd10256510179fe4df4a0fcc1090ba9c6165cdf
wade30822 <317560406@qq.com>
2024-12-25 18:43:36 -1000

    fix: set XDELTA3_FOUND to TRUE when find_package(xdelta3 REQUIRED) is successful

--- cmake/xdelta3Config.cmake
+++ cmake/xdelta3Config.cmake
@@ -4,6 +4,8 @@ set(XDELTA3_LIBRARIES "${CMAKE_INSTALL_PREFIX}/lib/lib${PROJECT_NAME}.a")
 # 设置对应的目标
 set(XDELTA3_TARGET xdelta3)
 
+set(XDELTA3_FOUND TRUE)
+
 # 为了兼容不同版本的 CMake，可以提供 find_package 时的配置选项
 find_package(PkgConfig)
 if(PKG_CONFIG_FOUND)
