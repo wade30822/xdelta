1fe6f5f2d1be5ae98f432adfd20ea15564d11d7b
wade30822 <317560406@qq.com>
2024-12-25 18:50:20 -1000

    fix: remove PkgConfig support

--- cmake/xdelta3Config.cmake
+++ cmake/xdelta3Config.cmake
@@ -5,11 +5,3 @@ set(XDELTA3_LIBRARIES "${CMAKE_INSTALL_PREFIX}/lib/lib${PROJECT_NAME}.a")
 set(XDELTA3_TARGET xdelta3)
 
 set(XDELTA3_FOUND TRUE)
-
-# 为了兼容不同版本的 CMake，可以提供 find_package 时的配置选项
-find_package(PkgConfig)
-if(PKG_CONFIG_FOUND)
-  pkg_check_modules(XDELTA3 REQUIRED xdelta3)
-  set(XDELTA3_INCLUDE_DIRS ${XDELTA3_INCLUDE_DIRS} ${XDELTA3_INCLUDE_DIRS})
-  set(XDELTA3_LIBRARIES ${XDELTA3_LIBRARIES} ${XDELTA3_LIBRARIES})
-endif()
